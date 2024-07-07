e1deeb56fe9dd5b3635f863e32e559f2dd541350
Arpan Gupta <arpan.gupta@lpu.in>
2024-07-08 03:55:09 +0530

    updated index.html

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Foreground from './components/foreground.jsx';
 function App() {
   return (
     <div className=" relative w-full h-screen bg-black text-white">
+      <h1>{import.meta.env.VITE_APP_TITLE}</h1>
       <Background />
       <Foreground/>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Docs-React-app/',
+  base: '/Docs-React-app/', 
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  define: {
+    'process.env.VITE_APP_TITLE': JSON.stringify('Arpan React Docs App')
+  }
 })
